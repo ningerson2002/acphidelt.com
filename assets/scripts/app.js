72ece04e757697c534be503621fc5fd73a70df34
ningerson2002 <ingersonmeacham01@allegheny.edu>
2024-10-08 20:55:53 -0400

    Finish mobile implementation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,19 @@
-document.querySelector(".menu-toggle").addEventListener("click", () => {
-  const navLinks = document.querySelectorAll(".nav-links");
-  navLinks.classList.toggle("active");
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
+
+  // Close menu when clicking outside of it
+  document.addEventListener("click", function (event) {
+    // Check if the click is outside the menu and the toggle button
+    if (
+      !navLinks.contains(event.target) &&
+      !menuToggle.contains(event.target)
+    ) {
+      navLinks.classList.remove("active");
+    }
+  });
 });
